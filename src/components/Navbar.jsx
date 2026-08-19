af491c312c9659aa06daa9ef97f51eb6f698f8b7
mohamed mohamed <mohameddkamall9@gmail.com>
2026-08-19 23:07:53 +0800

    fix some problems with navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+import { Result } from 'postcss';
 import logo from '../assets/logo.png';
 import {useState} from 'react';
 import { FiMenu, FiX } from 'react-icons/fi';
@@ -5,24 +6,42 @@ import { FiMenu, FiX } from 'react-icons/fi';
 const Navbar = () => {
     const [isopen , setopen] = useState(false);
 
+
+    const Menuchanger  = () => {
+    if (isopen == true ){
+    setopen(false)
+    } else {
+        setopen(true)
+    }
+
+    Result = isopen;
+
+    return Result;
+    
+    };
+
     return(
         <div className="navbar flex flex-row p-4 justify-between items-center bg-emerald-400 rounded-b-2xl shadow-md"> 
         <div className="navbar-logo  mr-10">
             <img className="w-16 h-16" src={logo} alt="Logo" />
         </div>
         <div className="navbar-links ">
-            <ul className="flex flex-row gap-4  text-lg font-semibold text-slate-100">
+            <ul className="flex flex-row gap-4  text-lg font-semibold text-slate-100 ">
                 <li><a href="">Home</a></li>
                 <li><a href="">About</a></li>
                 <li><a href="">Contact</a></li>
             </ul>
         </div>
-        {isopen ? (
-            <FiX className="navbar-menu-icon w-12 h-12" onClick={() => setopen(false)} ><div className='nav-window bg-emerald-700 w-3/6 h-screen flex flex-col'></div></FiX>
+        <div className="navbar-menu-button flex flex-row gap-4 ">
+            {isopen ? (
+            <FiX className="navbar-menu-icon w-12 h-12 cursor-pointer" onClick={Menuchanger} ><div className='nav-window bg-emerald-700 w-3/6 h-screen flex flex-col'></div></FiX>
         ) : (
-            <FiMenu className="navbar-menu-icon w-12 h-12" onClick={() => setopen(true)} />
+            <FiMenu className="navbar-menu-icon w-12 h-12 cursor-pointer" onClick={Menuchanger} />
         )}
-        <button className="navbar-button bg-yellow-400 text-black font-semibold py-2 px-4 rounded-lg shadow-md hover:bg-yellow-500 transition duration-300"><a></a>Sign Up/login</button>
+                <button className="navbar-button bg-yellow-400 text-black font-semibold py-2 px-4 rounded-lg shadow-md hover:bg-yellow-500 transition duration-300"><a></a>Sign Up/login</button>
+
+        </div>
+        
         
     </div>
     
