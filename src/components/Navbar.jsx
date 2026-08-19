790f4d2ea4f97659f27220882aad4b1ea72d92f9
mohamed mohamed <mohameddkamall9@gmail.com>
2026-08-20 00:34:38 +0800

    add the menu silde feature so I can make the navbar more responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,15 +21,15 @@ const Navbar = () => {
     };
 
     return(
-        <div className="navbar flex flex-row p-4 justify-between items-center bg-emerald-400 rounded-b-2xl shadow-md"> 
+        <div className="navbar flex flex-row p-4 justify-between items-center bg-emerald-400  shadow-md relative"> 
         <div className="navbar-logo  mr-10">
             <img className="w-16 h-16" src={logo} alt="Logo" />
         </div>
-        <div className="navbar-links ">
+        <div className="navbar-links hidden sm:flex  ">
             <ul className="flex flex-row gap-4  text-lg font-semibold text-slate-100 ">
-                <li><a href="">Home</a></li>
-                <li><a href="">About</a></li>
-                <li><a href="">Contact</a></li>
+                <li className="hover:text-green-300 transition duration-300"><a href="">Home</a></li>
+                <li className="hover:text-green-300 transition duration-300"><a href="">About</a></li>
+                <li className="hover:text-green-300 transition duration-300"><a href="">Contact</a></li>
             </ul>
         </div>
         <div className="navbar-menu-button flex flex-row gap-4 ">
@@ -39,9 +39,16 @@ const Navbar = () => {
             <FiMenu className="navbar-menu-icon w-12 h-12 cursor-pointer" onClick={Menuchanger} />
         )}
                 <button className="navbar-button bg-yellow-400 text-black font-semibold py-2 px-4 rounded-lg shadow-md hover:bg-yellow-500 transition duration-300"><a></a>Sign Up/login</button>
-
         </div>
-        
+        {isopen ? (<div className="nav-window bg-emerald-500 absolute top-full left-0 w-full rounded-b-2xl shadow-md">
+            <div className="navbar-links  ">
+            <ul className="flex  flex-col gap-4  text-lg font-semibold text-slate-100 ">
+                <li className="hover:text-green-300 transition duration-300"><a href="">Home</a></li>
+                <li className="hover:text-green-300 transition duration-300"><a href="">About</a></li>
+                <li className="hover:text-green-300 transition duration-300"><a href="">Contact</a></li>
+            </ul>
+        </div>
+        </div>) : (<div className="nav-window hidden "></div>)}
         
     </div>
     
