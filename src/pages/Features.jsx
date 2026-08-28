006e7d136a106906c963916a084834e6bf584d2a
mohamed mohamed <mohameddkamall9@gmail.com>
2026-08-28 19:24:54 +0800

    designed the frame for the feature card still need some fix but overall shape should be fine

--- src/pages/Features.jsx
+++ src/pages/Features.jsx
@@ -1,14 +1,24 @@
-import Navbar from '../components/Navbar'
-const Features = () => {
-    return (
-        <>
-            <Navbar />
-            <div className="features-container">
-                <h1>Our Features</h1>
-                <p>Here are the features of our task management app:</p>
-            </div>
-        </>
-    )
-}
+            import Navbar from '../components/Navbar'
+            const Features = () => {
+                return (
+                    <>
+                        <Navbar />
+                        <div className="features-container">
+                            <div className="feature-header flex items-center justify-center p-3">
+                                <h1 className="text-4xl font-extrabold"> Features</h1>
 
-export default Features
+                            </div>
+                            <div className="feature-body flex flex-row gap-4 justify-center items-center p-4 w-max h-max">
+                                <div className="feature-item1 bg-gray-300 h-100 w-80 m-3 rounded-md"></div>
+                                <div className="feature-item1 bg-gray-300 h-100 w-80 m-3 rounded-md">
+                                   
+                                </div>
+
+
+                            </div>
+                        </div>
+                    </>
+                )
+            }
+
+            export default Features
